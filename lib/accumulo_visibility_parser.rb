b60495d86751319f29ef17b59049d2d86a7ac220
Michael Wall <mjwall@gmail.com>
2014-05-12 16:52:02 -0400

    Start of building a parser

--- lib/accumulo_visibility_parser.rb
+++ lib/accumulo_visibility_parser.rb
@@ -1,5 +1,18 @@
 require "accumulo_visibility_parser/version"
+require 'parslet'
 
 module AccumuloVisibilityParser
-  # Your code goes here...
+  class VisiblityParser < Parslet::Parser
+    rule(:vis) { match('[a-zA-Z0-9_-]').repeat(1) }
+    rule(:pipe) { match("[|]") }
+    rule(:ampersand) { match("[&]")}
+    rule(:or_expr) { vis >> (pipe >> vis).repeat(1) }
+    rule(:and_expr) { vis >> (ampersand >> vis).repeat(1) }
+    rule(:expression) { vis | or_expr | and_expr }
+    root(:expression)
+  end
+
+  def self.parse visibility_string
+    VisiblityParser.new.parse(visibility_string)
+  end
 end
--- spec/accumulo_visibility_parser_spec.rb
+++ spec/accumulo_visibility_parser_spec.rb
@@ -3,10 +3,26 @@ require File.expand_path '../spec_helper.rb', __FILE__
 class AccumuloVisibilityParserSpec < MiniTest::Spec
 
   before do
-    puts "Before"
+    #puts "Before"
   end
 
-  it "should be tested" do
-    "A".must_equal "A"
+  it "should parse A" do
+    str = "A"
+    AccumuloVisibilityParser.parse(str).str.must_equal str
   end
+
+  it "should parse A|B" do
+    str = "A|B"
+    AccumuloVisibilityParser.parse(str).str.must_equal str
+  end
+
+  it "should parse (A|B)&(C|D)"
+  it "should parse orange|(red&yellow)"
+  it "should NOT parse A|B&C"
+  it "should NOT parse A=B"
+  it "should NOT parse A|B|"
+  it "should NOT parse A&B"
+  it "should NOT parse ()"
+  it "should NOT parse )"
+  it "should NOT parse dog|!cat"
 end
